c0110aa21c52fe973913432a946dbbda53779cbd
forough1370 <61025528+forough1370@users.noreply.github.com>
2020-04-01 19:34:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	console.log('script loaded!');
+
+	//If clicked true,change compress the 'type your name' text.
+    $('.True').click(function(){
+       $('').show();
+   });
