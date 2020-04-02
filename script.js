2160882caf1ccc9c1ecbbfdf1039c39193da37db
forough1370 <61025528+forough1370@users.noreply.github.com>
2020-04-02 13:00:36 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
 	console.log('script loaded!');
 
 	//If clicked true,change compress the 'type your name' text.
-    $('.True').click(function(){
-       $('').show();
+   //  $('.True').click(function(){
+   //     $('').show();
+   // });
+   let count = 0;
+   $('input[type=radio]').on('change', function() {
+   		console.log('input changed!');
+   		let value = $(this).val();
+
+   		if (value == "True"){
+   			console.log("True is selected");
+   			count++;
+   			
+   			weightValue = count*1000;
+
+   			let setting = "'wght'" + weightValue;
+
+   			$('.typehere').css('font-variation-settings', setting);
+   		}
+   	
+      // alert($('input[name=radioName]:checked', '#myForm').val()); 
    });
+
+});
